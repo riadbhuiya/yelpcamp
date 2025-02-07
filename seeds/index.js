ee9660da610447c4c578b80acb5d1804e0970861
riad bhuiya <riadbhuiya828@gmail.com>
2025-02-07 11:45:21 +0600

    reseed databse with images, styles and code updated

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const r = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[r].city}, ${cities[r].state}`
+            location: `${cities[r].city}, ${cities[r].state}`,
+            price,
+            image: `https://picsum.photos/600/400.jpg`,
+            description: `  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolor consequatur esse eos officia quo tempora enim dignissimos quibusdam laudantium fugit iusto similique, soluta at ea nemo mollitia libero non.
+                            Odit, assumenda deleniti ea unde, atque impedit debitis consequatur rerum nobis nisi itaque! Est deserunt officiis id nisi hic nesciunt. Nisi sapiente, obcaecati voluptatibus suscipit molestiae voluptatem quasi tenetur ad?
+                            Laboriosam ratione nihil sed ut nobis sapiente, quo tempora rerum rem iure voluptatum consequuntur atque blanditiis vel numquam aut ullam, incidunt est autem pariatur vitae. Et deserunt debitis vero nemo.`,
         })
         //console.log(camp)
         await camp.save()
