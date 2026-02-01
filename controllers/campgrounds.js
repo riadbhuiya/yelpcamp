5080190bf960f539cbc4437edadb5c853927205c
Riad Bhuiya <riadbhuiya828@gmail.com>
2026-02-01 18:07:34 +0600

    campground images delete form added

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -13,10 +13,10 @@ module.exports.createCampground = async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError("Invalid campground data.", 400);
     const campground = new Campground(req.body.campground);
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    console.log(`files map: ${campground.images}`)
+    // console.log(`files map: ${campground.images}`)
     campground.author = req.user._id;
     await campground.save();
-    console.log(`after save: ${campground}`)
+    // console.log(`after save: ${campground}`)
     req.flash("success", "Successfully created a new camp.");
     res.redirect(`/campgrounds/${campground._id}`)
 }
@@ -48,6 +48,7 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res, next) => {
     const { id } = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     const images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...images);
